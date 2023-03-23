d46a11a9b02cd2261171e5ee4bb3a74e76bf72cc
0xAckerMan <joelkores1@gmail.com>
2023-03-23 22:21:28 +0300

    Intro

---
+++ warmup/1-warmup.js
@@ -0,0 +1,19 @@
+// Comments can make code readable
+// This is a 30 days challenge
+ 
+
+/*
+This is a multi line comment
+as you see
+*/
+
+let a = "Name"; //string
+let b = 40; //number
+let c = false; //boolean
+let d; //undefined
+
+console.log(typeof d)
+console.log(typeof c)
+console.log(typeof b)
+console.log(typeof a)
+
