2b5b0c58e6b56fc890d038c225712af86eb1be75
0xAckerMan <joelkores1@gmail.com>
2023-03-23 22:21:52 +0300

    JS Data types

---
+++ warmup/2-data_types.js
@@ -0,0 +1,28 @@
+/*
+we have primitive and none primitive data types
+
+primitive- they are immutable(Non modifiable) {
+    numbers
+    strings
+    boolean
+    Null
+    Undefined
+    symbol
+}
+
+non primitive- are modifiable{
+    Objects
+    arrays
+}
+*/
+
+let challenge = "30 days of js"
+console.log(challenge)
+console.log(challenge.length)
+console.log(challenge.toUpperCase())
+console.log(challenge.toLowerCase())
+console.log(challenge.substring(3))
+console.log(challenge.slice(0,2))
+console.log(challenge.includes("script"))
+console.log(challenge.split())
+console.log(challenge.replace('js', 'py'))
